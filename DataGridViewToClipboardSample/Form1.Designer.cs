ea57dff73c534a1327f6accdf5415e113cfda389
Shingo Takahashi <cross2415@gmail.com>
2020-02-11 10:36:29 +0900

    処理を実装

--- DataGridViewToClipboardSample/Form1.Designer.cs
+++ DataGridViewToClipboardSample/Form1.Designer.cs
@@ -29,12 +29,107 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dataGridView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(661, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(203, 26);
+            // 
+            // ToolStripMenuItem
+            // 
+            this.ToolStripMenuItem.Name = "ToolStripMenuItem";
+            this.ToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.ToolStripMenuItem.Text = "一覧をクリップボードへコピー";
+            this.ToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "操作";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column1";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column2";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column3";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Column4";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Column5";
+            this.Column6.Name = "Column6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(689, 186);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
     }
 }
 
--- DataGridViewToClipboardSample/Form1.cs
+++ DataGridViewToClipboardSample/Form1.cs
@@ -15,6 +15,68 @@ namespace DataGridViewToClipboardSample
         public Form1()
         {
             InitializeComponent();
+
+            dataGridView1.Rows.Add("操作列01", "hoge01_1", "hoge01_2", "hoge01_3", "hoge01_4", "hoge01_5");
+            dataGridView1.Rows.Add("操作列02", "hoge02_1", "hoge02_2", "hoge02_3", "hoge02_4", "hoge02_5");
+            dataGridView1.Rows.Add("操作列03", "hoge03_1", "hoge03_2", "hoge03_3", "hoge03_4", "hoge03_5");
+            dataGridView1.Rows.Add("操作列04", "hoge04_1", "hoge04_2", "hoge04_3", "hoge04_4", "hoge04_5");
+            dataGridView1.Rows.Add("操作列05", "hoge05_1", "hoge05_2", "hoge05_3", "hoge05_4", "hoge05_5");
+            dataGridView1.Rows.Add("操作列06", "hoge06_1", "hoge06_2", "hoge06_3", "hoge06_4", "hoge06_5");
+            dataGridView1.Rows.Add("操作列07", "hoge07_1", "hoge07_2", "hoge07_3", "hoge07_4", "hoge07_5");
+            dataGridView1.Rows.Add("操作列08", "hoge08_1", "hoge08_2", "hoge08_3", "hoge08_4", "hoge08_5");
+            dataGridView1.Rows.Add("操作列09", "hoge09_1", "hoge09_2", "hoge09_3", "hoge09_4", "hoge09_5");
+            dataGridView1.Rows.Add("操作列10", "hoge10_1", "hoge10_2", "hoge10_3", "hoge10_4", "hoge10_5");
+
+            dataGridView1.Rows[0].Selected = true;
+        }
+
+        private void ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // 一時的に制御を解除
+
+            // 操作列以外をクリップボードにコピーするため、セル選択モード
+            // ※通常時は行全体を選択
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.CellSelect;
+
+            // 全行クリップボードへコピーするため、複数選択可
+            // ※通常は単一行のみ選択可
+            dataGridView1.MultiSelect = true;
+
+            int index = 0;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                foreach (DataGridViewCell column in row.Cells)
+                {
+                    // １列目はスキップ
+                    if (column.ColumnIndex == 0)
+                    {
+                        continue;
+                    }
+
+                    // 列選択
+                    column.Selected = true;
+                }
+
+                if (row.Selected)
+                {
+                    // 選択済みの行を退避
+                    index = row.Index;
+                }
+
+                row.Selected = true;
+            }
+
+            // クリップボードへコピー
+            Clipboard.SetDataObject(dataGridView1.GetClipboardContent());
+
+            // 単一行のみ、行全体を選択状態に戻す
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.MultiSelect = false;
+
+            // 退避した選択済みの行を設定
+            IEnumerable<DataGridViewRow> rows = dataGridView1.Rows.OfType<DataGridViewRow>();
+            DataGridViewRow selectedRow = rows.Where(x => x.Index == index).Single();
+            selectedRow.Selected = true;
         }
     }
 }
